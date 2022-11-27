bdc5d914441d4530e881a5e3d308cbd73d1d5e15
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2022-11-27 07:27:06 +0000

    finish 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
+  get 'top' => 'homes#top'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
